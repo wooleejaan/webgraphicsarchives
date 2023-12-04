7a5db44df77c5d61d0539bfb6ce6021c8edce2ae
yongwoo <wooleejaan@gmail.com>
2023-12-04 22:27:24 +0900

    feat: Implement canvas boilerplate

---
+++ canvas/fireworks/index.js
@@ -0,0 +1,47 @@
+import CanvasOption from "./js/CanvasOption.js";
+
+class Canvas extends CanvasOption {
+  constructor() {
+    super();
+  }
+  init() {
+    this.canvasWidth = innerWidth;
+    this.canvasHeight = innerHeight;
+    this.canvas.width = this.canvasWidth * this.dpr;
+    this.canvas.height = this.canvasHeight * this.dpr;
+    this.ctx.scale(this.dpr, this.dpr);
+
+    this.canvas.style.width = this.canvasWidth + "px";
+    this.canvas.style.height = this.canvasHeight + "px";
+  }
+
+  render() {
+    let now, delta;
+    let then = Date.now();
+
+    const frame = () => {
+      requestAnimationFrame(frame);
+      now = Date.now();
+      delta = now - then;
+      if (delta < this.interval) return;
+
+      // 여기에 코드를 작성합니다.
+      this.ctx.fillRect(100, 100, 200, 200);
+
+      then = now - (delta % this.interval);
+    };
+    requestAnimationFrame(frame); // 여기서 1번만 실행해주면 이후에는 frame 함수 안에 있는 Raf가 계속 실행됩니다
+  }
+}
+
+const canvas = new Canvas();
+
+window.addEventListener("load", () => {
+  canvas.init();
+  canvas.render();
+});
+
+// 사이즈 변경될 때마다 init만 재실행
+window.addEventListener("resize", () => {
+  canvas.init();
+});
