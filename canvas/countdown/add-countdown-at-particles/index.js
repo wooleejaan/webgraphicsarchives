3911d40351d5ce6dd2811247d74bc1b60f48aa76
yongwoo <wooleejaan@gmail.com>
2023-12-17 00:36:18 +0900

    chore: Add countdown template

---
+++ canvas/countdown/add-countdown-at-particles/index.js
@@ -0,0 +1,65 @@
+import Particle from "./js/Particle.js";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const dpr = window.devicePixelRatio;
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
+const interval = 1000 / 60;
+
+const particles = [];
+
+function init() {
+  canvasWidth = innerWidth;
+  canvasHeight = innerHeight;
+
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
+  canvas.width = canvasWidth * dpr;
+  canvas.height = canvasHeight * dpr;
+  ctx.scale(dpr, dpr);
+}
+
+function createRing() {
+  const PARTICLE_NUM = 800;
+  for (let i = 0; i < PARTICLE_NUM; i++) {
+    particles.push(new Particle());
+  }
+}
+
+function render() {
+  let now, delta;
+  let then = Date.now();
+  const frame = () => {
+    requestAnimationFrame(frame);
+
+    now = Date.now();
+    delta = now - then;
+    if (delta < interval) return;
+    // delta가 interval보다 커지는 시점에 아래 로직으로 한 장면을 실행.
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update();
+      particles[i].draw(ctx);
+
+      if (particles[i].opacity < 0) particles.splice(i, 1);
+    }
+
+    then = now - (delta % interval);
+  };
+
+  requestAnimationFrame(frame);
+}
+
+window.addEventListener("load", () => {
+  init();
+  render();
+});
+
+window.addEventListener("resize", init);
+
+window.addEventListener("click", () => {
+  // 클릭 시 원을 그립니다.
+  createRing();
+});
