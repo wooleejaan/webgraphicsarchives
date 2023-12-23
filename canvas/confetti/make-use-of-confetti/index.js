1594d42c5435163ae46a7616fbaa400b02da07c2
yongwoo <wooleejaan@gmail.com>
2023-12-23 09:41:30 +0900

    feat: Add doccs of Particle confetti

--- canvas/confetti/make-use-of-confetti/index.js
+++ canvas/confetti/make-use-of-confetti/index.js
@@ -19,9 +19,10 @@ function init() {
   ctx.scale(dpr, dpr);
 }
 
-function confetti({ x, y, count, deg, colors }) {
+// shapes를 추가합니다.
+function confetti({ x, y, count, deg, colors, shapes, spread }) {
   for (let i = 0; i < count; i++) {
-    particles.push(new Particle(x, y, deg, colors));
+    particles.push(new Particle(x, y, deg, colors, shapes, spread));
   }
 }
 
@@ -29,18 +30,124 @@ function render() {
   let now, delta;
   let then = Date.now();
 
+  let deg = 0;
+
   const frame = () => {
     requestAnimationFrame(frame);
     now = Date.now();
     delta = now - then;
     if (delta < interval) return;
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    // 매 프레임마다 구현되도록합니다.
+
+    // deg를 선언해주고, 매 프레임마다 1씩 값을 키워주고
+    // 더해주면, 회전하면서 쏩니다.
+    deg += 1;
+
+    // (1)
+    // confetti({
+    //   x: 0, // 좌측
+    //   y: 0.5,
+    //   count: 5,
+    //   deg: -50, // 좌측 각도
+    // });
+    // confetti({
+    //   x: 1, // 우측
+    //   y: 0.5,
+    //   count: 5,
+    //   deg: -130, // 우측 각도
+    // });
+
+    // (2)
+    // confetti({
+    //   x: 0,
+    //   y: 0,
+    //   count: 5,
+    //   deg: 45,
+    // });
+
+    // (3)
+    // confetti({
+    //   x: 1,
+    //   y: 0,
+    //   count: 5,
+    //   deg: 135,
+    // });
+
+    // (4)
+    // confetti({
+    //   x: 0.5,
+    //   y: 0.5,
+    //   count: 5,
+    //   deg: 270, // 여기까지 하면, 위를 향해서 발사.
+    // });
+
+    // (5)
+    // confetti({
+    //   x: 0.5,
+    //   y: 0.5,
+    //   count: 5,
+    //   deg: 270,
+    //   spread: 180, // spread까지 180으로 추가하면, 원모양으로 발사.
+    // });
+
+    // (6)
+    // confetti({ // 정신없는 confetti
+    //   x: Math.random(),
+    //   y: Math.random(),
+    //   count: 5,
+    //   deg: 270,
+    //   spread: 180,
+    // });
+
+    // (7)
+    // confetti({ // 수직으로 쏘는 형태
+    //   x: 0.5,
+    //   y: 0.5,
+    //   count: 5,
+    //   deg: 270,
+    //   spread: 1,
+    // });
+
+    // (7)
+    confetti({
+      x: 0.5,
+      y: 0.5,
+      count: 5,
+      deg: 225 + deg,
+      spread: 1,
+    });
+    confetti({
+      x: 0.5,
+      y: 0.5,
+      count: 5,
+      deg: 90 + deg,
+      spread: 1,
+    });
+    confetti({
+      x: 0.5,
+      y: 0.5,
+      count: 5,
+      deg: 315 + deg,
+      spread: 1,
+    });
+    confetti({
+      x: 0.5,
+      y: 0.5,
+      count: 5,
+      deg: 270 + deg,
+      spread: 1,
+    });
 
     for (let i = particles.length - 1; i >= 0; i--) {
       particles[i].update();
       particles[i].draw(ctx);
 
       if (particles[i].opacity < 0) {
+        particles.splice(i, 1);
+      }
+      // 추가적인 처리를 통한 성능 최적화.
+      if (particles[i].y > canvasHeight) {
         particles.splice(i, 1);
       }
     }
@@ -56,7 +163,9 @@ window.addEventListener("click", () => {
     y: 0.5,
     count: 10,
     deg: -50,
-    colors: ["#FF0000"],
+    // colors: ["#FF0000"],
+    shapes: ["circle"],
+    spread: 1,
   });
 });
 window.addEventListener("resize", init);
--- canvas/confetti/make-use-of-confetti/js/Particle.js
+++ canvas/confetti/make-use-of-confetti/js/Particle.js
@@ -1,8 +1,9 @@
 import { hexToRgb, randomNumBetween } from "./utils.js";
 
 export default class Particle {
-  constructor(x, y, deg = 0, colors) {
-    this.angle = (Math.PI / 180) * randomNumBetween(deg - 30, deg + 30);
+  constructor(x, y, deg = 0, colors, shapes, spread = 30) {
+    // 커지는 각도를 30이 아니라 변수 spread로 받아서 유연하게 만들어줍니다.
+    this.angle = (Math.PI / 180) * randomNumBetween(deg - spread, deg + spread);
     this.r = randomNumBetween(30, 100);
     this.x = x * innerWidth;
     this.y = y * innerHeight;
@@ -27,8 +28,12 @@ export default class Particle {
 
     this.colors = colors || ["#FF577F", "#FF884B", "#FFD384", "FFF9B0"];
     this.color = hexToRgb(
-      this.colors[Math.floor(randomNumBetween(0, this.colors.length - 1))]
+      this.colors[Math.floor(randomNumBetween(0, this.colors.length))] // floor로 하면 length -1 이 아니라 length 여야 합니다.
     );
+
+    this.shapes = shapes || ["circle", "square"]; // shapes를 받아와 저장합니다.
+    this.shape =
+      this.shapes[Math.floor(randomNumBetween(0, this.shapes.length))]; // floor로 하면 length -1 이 아니라 length 여야 합니다.
   }
   update() {
     this.vy += this.gravity;
@@ -46,6 +51,32 @@ export default class Particle {
 
     this.rotation += this.rotationDelta;
   }
+  drawSquare(ctx) {
+    // fillRect를 여기 안으로 옮깁니다.
+    ctx.fillRect(
+      this.x,
+      this.y,
+      this.width * Math.cos((Math.PI / 180) * this.widthDelta),
+      this.height * Math.sin((Math.PI / 180) * this.heightDelta)
+    );
+  }
+  drawCircle(ctx) {
+    // 원형을 그려줍니다.
+    ctx.beginPath();
+    // arc가 반지름 하나로 원을 그린다면,
+    // ellipse는 반대로 반지름 2개를 가지고 타원을 그립니다.
+    ctx.ellipse(
+      this.x,
+      this.y,
+      Math.abs(this.width * Math.cos((Math.PI / 180) * this.widthDelta)) / 2, // radius x (음수가 나오면 안 됩니다. cos,sin은 -1까지도 가므로 abs를 적용해줍니다.)
+      Math.abs(this.height * Math.sin((Math.PI / 180) * this.heightDelta)) / 2, // radius y (square와 달리 반지름 값으로 이 랜덤값을 사용하므로 /2를 해줘야 사이즈가 맞습니다.)
+      0, // 타원을 시계방향으로 회전시키는 각도. (이미 radius x,y에서 cos,sin으로 회전을 주고 있으므로 0을 줍니다.)
+      0, // 시작하는 각도
+      Math.PI * 2 // 끝나는 각도
+    );
+    ctx.fill(); // fillStyle은 공통부분에서 그릴 것이므로 fill만 해줍니다.
+    ctx.closePath();
+  }
   draw(ctx) {
     ctx.save();
 
@@ -55,12 +86,23 @@ export default class Particle {
     ctx.translate(-this.x - this.width * 1.2, -this.y - this.height * 1.2);
 
     ctx.fillStyle = `rgba(${this.color.r}, ${this.color.g}, ${this.color.b}, ${this.opacity})`;
-    ctx.fillRect(
-      this.x,
-      this.y,
-      this.width * Math.cos((Math.PI / 180) * this.widthDelta),
-      this.height * Math.sin((Math.PI / 180) * this.heightDelta)
-    );
+
+    // 위에서 translate,rotate,fillStyle은 공통스타일이므로 여기에 작성합니다.
+    switch (this.shape) {
+      case "square":
+        this.drawSquare(ctx);
+        break;
+      case "circle":
+        this.drawCircle(ctx);
+        break;
+    }
+
+    // ctx.fillRect(
+    //   this.x,
+    //   this.y,
+    //   this.width * Math.cos((Math.PI / 180) * this.widthDelta),
+    //   this.height * Math.sin((Math.PI / 180) * this.heightDelta)
+    // );
     ctx.resetTransform();
     ctx.restore();
   }
