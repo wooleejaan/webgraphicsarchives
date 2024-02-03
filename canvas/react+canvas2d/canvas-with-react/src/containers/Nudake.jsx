4c2ad9f58906351572baef23bd8055f9a74fd6c5
yongwoo <wooleejaan@gmail.com>
2024-02-03 10:48:37 +0900

    feat: Implement Nudake animation

--- canvas/react+canvas2d/canvas-with-react/src/containers/Nudake.jsx
+++ canvas/react+canvas2d/canvas-with-react/src/containers/Nudake.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import throttle from "lodash/throttle";
+import throttle from "lodash/throttle"; // lodash를 전부 가져오는 게 아니라 throttle까지만 import해와서 용량을 줄여야 한다.
 import gsap from "gsap";
 import "../style/containers/Nudake.css";
 
@@ -21,25 +21,31 @@ const Nudake = () => {
     const canvasParent = canvas.parentNode;
     const ctx = canvas.getContext("2d");
 
+    // 이미지 배열
     const imageSrcs = [image1, image2, image3];
     const loadedImages = [];
-    let currIndex = 0;
+    let currIndex = 0; // currentImageIndex. 이미지 배열을 계속 반복하며 보여줄 것이므로.
     let prevPos = { x: 0, y: 0 };
     let isChanging = false;
 
     let canvasWidth, canvasHeight;
 
     function resize() {
+      // 캔버스 크기는 부모 크기만큼 잡히도록.
       canvasWidth = canvasParent.clientWidth;
       canvasHeight = canvasParent.clientHeight;
+      // 캔버스의 크기는 2가지(캔버스 자체, style)
       canvas.style.width = canvasWidth + "px";
       canvas.style.height = canvasHeight + "px";
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
+      // 이번엔 dpr 필요 없다. 이미지만 요소 안에 알맞게 렌더링하면 되므로.
 
+      // 이미지를 preload
       preloadImages().then(() => drawImage());
     }
 
+    // 이미지 그릴 때마다 요청하는 게 아니라 미리 요청해서 네트워크 환경 느릴 때도 괜찮도록
     function preloadImages() {
       return new Promise((resolve, reject) => {
         let loaded = 0;
@@ -55,19 +61,28 @@ const Nudake = () => {
       });
     }
 
+    // 이미지를 그린다.
     function drawImage() {
-      isChanging = true;
+      isChanging = true; // gsap으로 이미지를 그리는 동안에는 Mousemove 이벤트 동작 제한하기 위해(사이드 이펙트 방지)
       const image = loadedImages[currIndex];
+      // globalCompositeOperation
+      // 처음 그릴 떄는 source-over가 찍히고 그다음부터는 destination~이 찍히므로
+      // 이걸로 처음 그리는지 아닌지 판단.
       const firstDrawing = ctx.globalCompositeOperation === "source-over";
 
+      // 이미지 그릴 때 opacity 애니메이션으로 자연스럽게 불러오도록.
       gsap.to(canvas, {
         opacity: 0,
-        duration: firstDrawing ? 0 : 1,
+        duration: firstDrawing ? 0 : 1, // 처음 그릴 때는 gsap을 쓸 필요 없으므로.
         onComplete: () => {
           canvas.style.opacity = 1;
           ctx.globalCompositeOperation = "source-over";
+          // 이미지 그리는 걸 object-fit:cover로 통일.
           drawImageCenter(canvas, ctx, image);
 
+          // 지웠을 때, 그 다음 이미지가 보여야 하므로
+          // 부모 요소에 backgroundImage로 그 다음 index 이미지를 가져온다.
+          // 이미지는 한정되어 있으므로 % 연산자로. 매번 반복하도록.
           const nextImage = imageSrcs[(currIndex + 1) % imageSrcs.length];
           canvasParent.style.backgroundImage = `url(${nextImage})`;
           prevPos = null;
@@ -82,6 +97,7 @@ const Nudake = () => {
       canvas.addEventListener("mouseup", onMouseUp);
       canvas.addEventListener("mouseleave", onMouseUp);
       canvas.addEventListener("mousemove", onMouseMove);
+      // 마우스를 눌렀을 때 현재 값을 prevPos로 저장.
       prevPos = { x: e.offsetX, y: e.offsetY };
     }
 
@@ -100,9 +116,11 @@ const Nudake = () => {
     function drawCircles(e) {
       const nextPos = { x: e.offsetX, y: e.offsetY };
       if (!prevPos) prevPos = nextPos;
+      // 두 점 사이의 거리와 각도를 구한다.
       const dist = getDistance(prevPos, nextPos);
       const angle = getAngle(prevPos, nextPos);
 
+      // 거리만큼 1씩 증가하면서 이전 점과 현재 점 사이를 원으로 빈틈없이 채워준다.
       for (let i = 0; i < dist; i++) {
         const x = prevPos.x + Math.cos(angle) * i;
         const y = prevPos.y + Math.sin(angle) * i;
@@ -116,6 +134,10 @@ const Nudake = () => {
 
       prevPos = nextPos;
     }
+    // 지워진 영역 비율 체크를 매번 하면 성능 이슈 발생함.
+    // 매번 rgba로 이루어진 거대한 data 배열을 순회해야 하므로
+    // 그래서 여기에 throttle 함수를 적용해줘야 함.
+    // 개발자 도구의 performance monitor 보면 90% 대로 계속 유지됨. 매우 에바.
     const checkPercent = throttle(() => {
       const percent = getScrupedPercent(ctx, canvasWidth, canvasHeight);
       if (percent > 50) {
@@ -124,6 +146,12 @@ const Nudake = () => {
       }
     }, 500);
 
+    // react에서 requestAnimationFrame를 사용할 때, 주의점
+    // 다른 페이지로 넘어가거나 다른 컴포넌트로 교체되면서, 해당 컴포넌트가 언마운트될 때
+    // requestAnimationFrame은 재귀함수로 스스로 계속 실행하도록 뒀기 때문에
+    // 알아서 중단이 안 되므로, 컴포넌트는 언마운트되어도 안에 있는 requestAnimationFrame은 계속 실행되는 문제가 발생함.
+    // 이럴 때는 frameId = requestAnimationFrame(frame)로 선언하면 return으로 프레임id가 나오므로
+    // 이 frameId를 cancelAnimationFrame(frameId)와 같이 react useEffect의 return 문에 넣어주면 된다.
     canvas.addEventListener("mousedown", onMousedown);
     window.addEventListener("resize", resize);
     resize();
--- canvas/react+canvas2d/canvas-with-react/src/utils/utils.js
+++ canvas/react+canvas2d/canvas-with-react/src/utils/utils.js
@@ -1,4 +1,6 @@
 export function getDistance(p1, p2) {
+  // 두 점 사이의 거리를 먼저 구한다.
+  // 왜냐면 두 점 사이의 거리를 구해서 그 사이에 원들을 채워넣어서 점을 찍는 게 아니라 선을 그리는 모션을 구현할 것이므로
   const dx = p2.x - p1.x;
   const dy = p2.y - p1.y;
 
@@ -9,22 +11,33 @@ export function getAngle(p1, p2) {
   const dx = p2.x - p1.x;
   const dy = p2.y - p1.y;
 
+  // 두 점 사이의 절대적인 각도 구하기.
+  // 두 점 사이의 각도 구하는 게 Math.atan2로 사실상 공식화되어 있음.
   return Math.atan2(dy, dx);
 }
 
 export function getScrupedPercent(ctx, width, height) {
   const pixels = ctx.getImageData(0, 0, width, height);
-  const gap = 32;
+  // 캔버스가 1000x1000픽셀이라고 가정하면, 100만개의 픽셀이 존재함
+  // 100만개가 각각 4개(rgba) 값을 가지므로 data 배열의 길이가 400만개임.
+  // 모두 돌면 성능 문제가 생기므로, 듬성듬성 search만 해줘도 충분하므로 gap을 조절해서 성능을 높일 수 있다.
+  const gap = 32; // 4의 배수면 된다.
   const total = pixels.data.length / gap;
   let count = 0;
 
+  // 이미지를 getImageData를 해올 때, rgba이므로 4개씩 들어감. 3개 불러오면 data안에 12개가 들억감.
+  // rgb는 건너뛰고, 4번째가 a 투명도이므로,
+  // 4번째마다 있는 투명도를 체크해서 그게 0이면 지워졌다고 판단할 수 있다.
+  // count로 세면, 지워진 영역이 몇 퍼센트인지 확인할 수 있고, 이걸로 얼마나 지워졌는지 판단할 수 있다.
   for (let i = 0; i < pixels.data.length - 3; i += gap) {
+    // 전체 길이에서 투명도만 체크할거라서, i+3만 체크하고, 전체길이-3까지만 순회하면 됨.
     if (pixels.data[i + 3] === 0) count++;
   }
 
   return Math.round((count / total) * 100);
 }
 
+// js로 이미지 그리는 방식 통일(css의 object-fit:cover 구현)
 export function drawImageCenter(canvas, ctx, image) {
   const cw = canvas.width;
   const ch = canvas.height;
@@ -47,5 +60,6 @@ export function drawImageCenter(canvas, ctx, image) {
   sx = iw / 2 - sw / 2;
   sy = ih / 2 - sh / 2;
 
+  // 이미지를 object-fit:cover로 그리는 방법.
   ctx.drawImage(image, sx, sy, sw, sh, 0, 0, cw, ch);
 }
